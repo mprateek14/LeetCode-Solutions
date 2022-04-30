d3486f01d2018bbd12539b2dccb57828b607535c
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-05-01 00:23:45 +0530

    Time: 9 ms (9.41%), Space: 9.1 MB (31.47%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int n, vector<int>& nums2, int m) {
         
-        if(!nums2.size()) return;
         
-        int i = 0;
-        int j;
-        for(i=0; i<n; i++){
-            if(nums1[i]>nums2[0]){
-                swap(nums1[i], nums2[0]);
-                
-                int temp = nums2[0];
-                
-                for(j=1; j<m and nums2[j]<temp; j++){
-                    
-                        nums2[j-1] = nums2[j];
-                    
-                }
-                nums2[j-1] = temp;
+        int a = n-1;
+        int b = m-1;
+        int c = n+m-1;
+        
+        while(a>=0 and b>=0){
+            if(nums1[a]>nums2[b]){
+                nums1[c] = nums1[a];
+                a--;
+                c--;
             }
-            
-            for(int k=0; k<n; k++) cout << nums1[k] << " ";
-            cout << endl;
-                    for(int j=0; j<m; j++) cout << nums2[j] << " ";
-        cout << endl;
+            else{
+                nums1[c] = nums2[b];
+                b--;
+                c--;
+            }
         }
-        j = 0;
-        while(i<n+m){
-            nums1[i] = nums2[j];
-            i++;
-            j++;
-            
-            // for(int k=0; k<n+m; k++) cout << nums1[k] << " ";
-            // cout << endl;
+        
+        while(b>=0){
+            nums1[c] = nums2[b];
+            b--;
+            c--;
         }
         
     }
