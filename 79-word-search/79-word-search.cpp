5eb1d4b95e0dd35e26bc3e7cd37025c8dee42cc3
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-05-02 19:50:46 +0530

    Time: 1026 ms (11.30%), Space: 7.9 MB (75.37%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        int row = board.size();
+        int col = board[0].size();
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(helper(board, i, j, word, 0)) return true;
+            }
+        }
+        
+        return false;
+        
+    }
+    
+    bool helper(vector<vector<char>>& board, int i, int j, string word, int idx){
+        if(idx == word.length()) return true;
+        
+        if(i<0 or i>=board.size() or j<0 or j>=board[0].size() or board[i][j]!=word[idx]) return false;
+        
+        
+        char c = board[i][j];
+        board[i][j] = '.';
+        bool res = helper(board, i+1, j, word, idx+1) or helper(board, i-1, j, word, idx+1) or helper(board, i, j+1, word, idx+1) or helper(board, i, j-1, word, idx+1);
+        board[i][j] = c;
+        
+        return res;
+    }
+    
+};
