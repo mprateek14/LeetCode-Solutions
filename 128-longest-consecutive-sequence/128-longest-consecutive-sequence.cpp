545d350a1b25a3d73170fee02dfadcb6f0f305aa
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-05-03 00:22:37 +0530

    Time: 936 ms (9.41%), Space: 28.9 MB (81.53%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set <int> hash(nums.begin(), nums.end());
+        //Brute force by sorting and checking max continuity
+        //optimise by hashing the values in set. Now check for each if a value-1 exists.
+        // if it does that means it is a part of bigger sequence. else start counting from there.
         
+        
+        
+        unordered_set<int> seq(nums.begin(), nums.end());
         int maxi = 0;
         
         for(int i: nums){
-            if(hash.count(i-1)) continue;
+            if(seq.count(i-1)) continue;
             else{
                 int j = i+1;
-                int sum = 1;
-                while(hash.count(j)){
+                int sum = 1;    //i already exists
+                while(seq.count(j)){
                     sum++;
                     j++;
                 }
+                
                 maxi = max(sum, maxi);
             }
         }
         
         return maxi;
-        
     }
 };
