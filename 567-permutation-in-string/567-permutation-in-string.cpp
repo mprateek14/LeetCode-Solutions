5c9c60ad14ef38587085ce390eff6fee184fd5af
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-05-03 17:19:45 +0530

    Time: 12 ms (58.16%), Space: 7.3 MB (75.45%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        int n = s1.length();
+        int m = s2.length();
+        
+        if(n>m) return false;
+        
+        vector<int> cur(26,0);
+        vector<int> goal(26,0);
+        
+        for(char c: s1){
+            cur[c-'a']++;
+        }
+        
+        for(int i=0; i<m; i++){
+            goal[s2[i]-'a']++;
+            if(i>=n) goal[s2[i-n] - 'a']--;
+            if(cur == goal) return true;
+        }
+        
+        return false;
+    }
+};
