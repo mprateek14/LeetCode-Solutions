f0278955aae535783a1030091163f7f0cad2c54b
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-05-07 21:52:17 +0530

    Time: 68 ms (7.62%), Space: 13.3 MB (10.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,19 +4,19 @@ public:
         int n = text1.length();
         int m = text2.length();
         
-        // vector<vector<int>> mem(n+1, vector<int>(m+1, -1));
-        // return memo(text1, n, text2, m, mem);
+        vector<vector<int>> mem(n, vector<int>(m, -1));
+        return memo(text1, n-1, text2, m-1, mem);
         
-        return lcsdp(text1, n, text2, m);
+        // return lcsdp(text1, n, text2, m);
         
     }
     
     int memo(string &s1, int n, string &s2, int m, vector<vector<int>> &mem){
-        if(n==0 or m==0) return 0;
+        if(n<0 or m<0) return 0;
         
         if(mem[n][m] != -1) return mem[n][m];
         
-        if(s1[n-1] == s2[m-1]) return mem[n][m] =  1 + memo(s1, n-1, s2, m-1, mem);
+        if(s1[n] == s2[m]) return mem[n][m] =  1 + memo(s1, n-1, s2, m-1, mem);
         else{
             return mem[n][m] = max(memo(s1, n-1, s2, m, mem), memo(s1, n, s2, m-1, mem));
         }
