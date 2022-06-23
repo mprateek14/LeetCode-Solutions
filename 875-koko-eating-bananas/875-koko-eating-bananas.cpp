5044229589199bc31b19d362e9fd82741fe0a46d
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-06-24 00:43:42 +0530

    Time: 35 ms (99.45%), Space: 18.9 MB (67.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -15,10 +15,7 @@ public:
                 sum += (i + mid -1)/mid;
             }
             
-            if(sum<=h){
-                
-                high = mid;
-            }
+            if(sum<=h) high = mid;
             else low = mid+1;
         }
         
@@ -26,3 +23,9 @@ public:
         
     }
 };
+
+// FIRST OF ALL TO EAT ALL BANANAS, SPEED MUST BE >= THAN THE SIZE OF PILE SINCE AT MOST 1 PILE CAN BE EATEN AT A TIME. WE HAVE TO FIND BANANAS EATEN PER HOUR. SO OUR SEARCH SPACE WILL BECOME FROM 1 TO MAX ELEMENT IN PILE.
+
+// NOW WE CAN BRUTEFORCE SOLUTION STARTING FROM 1 AND CHECKING IF BANANAS GET FINISHED IN h HOURS.
+
+// LOOP IS BASICALLY CALC TOTAL HOURS. i+mid-1 MAKES THE VALUE GO TO GREATER SIDE. SUPPOSE 5 BANANAS PER HOUR AND WE HAVE TO CHECK FOR 11. 11/5 GIVES 2. BUT WE NEED IT TO BE 3. SO WE USE LITTLE MATHS FOR IT
