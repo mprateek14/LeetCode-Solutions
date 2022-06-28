2db670d80bd0785a493f28d0e33504727470fd07
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-06-28 17:32:49 +0530

    Time: 16 ms (78.11%), Space: 10.6 MB (85.34%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+        int n = str1.length();
+        int m = str2.length();
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0; i<n+1; i++) dp[i][0] = 0;
+        for(int j=0; j<m+1; j++) dp[0][j] = 0;
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(str1[i-1] == str2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        
+        string ans;
+        
+        int i = n;
+        int j = m;
+        
+        while(i>0 and j>0){
+            if(str1[i-1] == str2[j-1]){
+                ans+=str1[i-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1]){
+                ans += str1[i-1];
+                i--;
+            }
+            else{
+                ans += str2[j-1];
+                j--;
+            }
+        }
+        
+        
+        while(i>0){
+            ans += str1[i-1];
+            i--;
+        }
+        
+        while(j>0){
+            ans += str2[j-1];
+            j--;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+        
+    }
+};
