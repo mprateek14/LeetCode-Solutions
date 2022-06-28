b7a8fdce73b62f32d7083a2378af86aed5c94e39
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-06-28 18:05:33 +0530

    Time: 31 ms (87.76%), Space: 23.9 MB (33.76%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -6,10 +6,8 @@ public:
         
         long double dp[n+1][m+1];
         
-        for(int j=0; j<m+1; j++) dp[0][j] = 0;
-        for(int i=0; i<n+1; i++) dp[i][0] = 1;
-        
-        
+        for(int i=0; i<m+1; i++) dp[0][i] = 0;
+        for(int j=0; j<n+1; j++) dp[j][0] = 1;
         
         for(int i=1; i<n+1; i++){
             for(int j=1; j<m+1; j++){
@@ -19,7 +17,6 @@ public:
                 else dp[i][j] = dp[i-1][j];
             }
         }
-        
         
         return dp[n][m];
         
