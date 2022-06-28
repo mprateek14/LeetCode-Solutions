bfd392de9ecb8e4bc743de79c15e2eddee7aab7c
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-06-28 18:14:11 +0530

    Time: 148 ms (56.66%), Space: 10.7 MB (77.77%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -9,7 +9,7 @@ public:
         int dp[n+1][n+1];
         
         for(int i=0; i<n+1; i++) dp[i][0] = 0;
-        for(int j=0; j<n+1; j++) dp[0][j] = 0;
+        for(int i=0; i<n+1; i++) dp[0][i] = 0;
         
         for(int i=1; i<n+1; i++){
             for(int j=1; j<n+1; j++){
@@ -18,33 +18,7 @@ public:
             }
         }
         
-        // CODE FOR PRINTING LCS
-        
-//         int len = dp[n][n];
-//         int idx = len-1;
-        
-//         string ans = "";
-//         for(int i=0; i<len; i++) ans+="$";
-        
-//         int i=n;
-//         int j=n;
-        
-//         while(i>0 and j>0){
-//             if(s[i-1] == t[j-1]){
-//                 ans[idx] = s[i-1];
-//                 idx--;
-//                 i--;
-//                 j--;
-//             }
-            
-//             else if(dp[i-1][j] > dp[i][j-1]){
-//                 i--;
-//             }
-//             else j--;
-//         }
-        
-//         cout << ans;
-        
         return dp[n][n];
+        
     }
 };
