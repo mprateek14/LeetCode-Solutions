4cc48c0935d1d872b492b8bfe92fb4cb9e57d25c
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-07-14 17:34:49 +0530

    Time: 161 ms (5.02%), Space: 27.6 MB (8.06%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<int> dir = {-1,0,1,0,-1};
+            
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        
+        int ans = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 1 and !vis[i][j]){
+                    bfs(grid, i, j, n, m, vis, dir, ans);
+                    vis[i][j] = 1;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+    void bfs(vector<vector<int>> &grid, int i, int j, int n, int m, vector<vector<int>> &vis, vector<int> &dir, int &ans){
+        
+        queue<pair<int, int>> q;
+        int sum = 1;
+        
+        q.push({i, j});
+        vis[i][j] = 1;
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            for(int k=0; k<4; k++){
+                int r = row + dir[k];
+                int c = col + dir[k+1];
+                
+                if(r>=0 and r<n and c>=0 and c<m and grid[r][c] == 1 and !vis[r][c]){
+                    cout << r << " " << c << endl;
+                    q.push({r,c});
+                    sum++;
+                    vis[r][c] = 1;
+                }
+            }
+            
+        }
+            ans = max(ans, sum);
+            cout << "ans for " <<i << " " << j << " is" << ans << endl;
+    }
+    
+    
+};
