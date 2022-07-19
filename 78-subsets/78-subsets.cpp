d7b4ce6bfc5d32ea058b6a1c75bcbc9481539984
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-07-20 01:07:36 +0530

    Time: 6 ms (39.17%), Space: 7.2 MB (56.19%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,24 +3,23 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
         vector<vector<int>> ans;
-        int size = nums.size()-1;
         
-        subset(nums, temp, 0, size, ans);
+        subset(nums, 0, temp, ans);
         
         return ans;
     }
     
-    void subset(vector<int> &arr, vector<int> temp, int start, int size, vector<vector<int>> &ans){
-        if(start>size){
-            ans.push_back(temp);
-            return;
-        }
-                subset(arr, temp, start+1, size, ans);
-
-        temp.push_back(arr[start]);
-        subset(arr, temp, start+1, size, ans);
+    void subset(vector<int> &arr, int idx, vector<int> temp, vector<vector<int>> &ans){
         
-        // temp.pop_back();
+        ans.push_back(temp);
+        
+        for(int i=idx; i<arr.size(); i++){
+            temp.push_back(arr[i]);
+            subset(arr, i+1, temp, ans);
+            temp.pop_back();
+        }
            
     }
+    
+    
 };
