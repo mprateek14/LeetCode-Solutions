f3c70e407936b413c22bfd5fc5778c9f180ec052
Prateek Madan <36787357+mprateek14@users.noreply.github.com>
2022-07-21 00:52:18 +0530

    Time: 480 ms (58.34%), Space: 8.1 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -5,9 +5,11 @@ public:
         int row = board.size();
         int col = board[0].size();
         
+        vector<int> dir = {-1,0,1,0,-1};
+        
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(helper(board, i, j, word, 0)) return true;
+                if(helper(board, i, j, word, 0, dir)) return true;
             }
         }
         
@@ -15,7 +17,7 @@ public:
         
     }
     
-    bool helper(vector<vector<char>>& board, int i, int j, string &word, int idx){
+    bool helper(vector<vector<char>>& board, int i, int j, string &word, int idx, vector<int> &dir){
         if(idx == word.length()) return true;
         
         if(i<0 or i>=board.size() or j<0 or j>=board[0].size() or board[i][j]!=word[idx]) return false;
@@ -23,10 +25,15 @@ public:
         
         char c = board[i][j];
         board[i][j] = '.';
-        bool res = helper(board, i+1, j, word, idx+1) or helper(board, i-1, j, word, idx+1) or helper(board, i, j+1, word, idx+1) or helper(board, i, j-1, word, idx+1);
+        
+        for(int k=0; k<4; k++){
+            if(helper(board, i+dir[k], j+dir[k+1], word, idx+1, dir)) return true;
+        }
+        
+        // bool res = helper(board, i+1, j, word, idx+1) or helper(board, i-1, j, word, idx+1) or helper(board, i, j+1, word, idx+1) or helper(board, i, j-1, word, idx+1);
         board[i][j] = c;
         
-        return res;
+        return false;
     }
     
 };
